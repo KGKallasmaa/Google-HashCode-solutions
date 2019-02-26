30314b2f088589199255660238392683fb66b5e8
Karl-Gustav Kallasmaa <karl.gustav1789@gmail.com>
2019-02-26 19:43:18 +0200

    -

--- 2019/main.py
+++ 2019/main.py
@@ -9,8 +9,28 @@ def read_general_data (fname):
     with open(fname) as f:
         content = f.readlines()
         first_row = content[0]
-        print (first_row)
+       ## first_row = int(d) for d in str(first_row)
+       # r,c,l,h = first_row.split()
+        return first_row.split()
 
+def read_pizza (fname):
+    '''
+    Return:
+    
+    '''
+    with open(fname) as f:
+        content = f.readlines()
+        rows = content[1:]
+
+        pizza = []
+        
+        for row in rows:
+            splited_row = list(row)
+            del splited_row[-1]
+           
+            pizza.append(splited_row)
+
+        return pizza    
 def efectivness(points,l,r):
     return (points)/(l*r)
 
@@ -18,4 +38,7 @@ def efectivness(points,l,r):
 
 #Input
 file_name = "files/example.in"
-read_general_data(file_name)
+r,c,l,h = read_general_data(file_name)
+pizza = read_pizza(file_name)
+
+print (pizza)
